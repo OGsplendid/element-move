f9576bd11484a86af7fee2c5e1e4eacf3001b337
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-06-07 21:37:29 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+const cells = [...document.querySelectorAll('.cell')];
+
+const goblinElement = document.createElement('img');
+goblinElement.src = 'https://github.com/netology-code/ahj-homeworks/blob/AHJ-50/dom/pic/goblin.png?raw=true';
+
+// функция ищет индекс != индексу активного элемента
+function getProperIndex() {
+  const activeCell = cells.find((el) => el.classList.contains('active'));
+  const activeCellIndex = cells.indexOf(activeCell);
+  let index = Math.floor(Math.random() * 16);
+  if(activeCellIndex === index) {
+    getProperIndex();
+  }
+  return index;
+}
+
+setInterval(() => {
+  const index = getProperIndex();
+  cells.forEach((el) => el.classList.remove('active'));
+  cells[index].classList.add('active');
+  cells[index].appendChild(goblinElement);
+}, 2000);
