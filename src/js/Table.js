c289275c43c9a8d5f0235750ecbac0deea420a48
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-08-01 09:50:31 +0300

    first version

---
+++ src/js/Table.js
@@ -0,0 +1,39 @@
+export default class Table {
+  constructor(element) {
+    try {
+      this.element = element;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  createGoblinElement() {
+    const goblinElement = document.createElement('img');
+    goblinElement.classList.add('goblin');
+    goblinElement.src = 'https://github.com/netology-code/ahj-homeworks/blob/AHJ-50/dom/pic/goblin.png?raw=true';
+    return goblinElement;
+  }
+
+  findProperIndex() {
+    const cells = [...this.element.querySelectorAll('.cell')];
+    const activeCell = cells.find((el) => el.classList.contains('active'));
+    const activeCellIndex = cells.indexOf(activeCell);
+    const index = Math.floor(Math.random() * 16);
+
+    if (activeCellIndex === index) {
+      this.findProperIndex();
+    }
+    return index;
+  }
+
+  rotate() {
+    const cells = [...this.element.querySelectorAll('.cell')];
+    const index = this.findProperIndex();
+    const goblinElement = this.createGoblinElement();
+    setInterval(() => {
+      cells.forEach((el) => el.classList.remove('active'));
+      cells[index].classList.add('active');
+      cells[index].appendChild(goblinElement);
+    }, 1000);
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,4 @@
-const cells = [...document.querySelectorAll('.cell')];
+import Table from './Table';
 
-const goblinElement = document.createElement('img');
-goblinElement.src = 'https://github.com/netology-code/ahj-homeworks/blob/AHJ-50/dom/pic/goblin.png?raw=true';
-goblinElement.classList.add('goblin');
-
-// функция ищет индекс != индексу активного элемента
-function getProperIndex() {
-  const activeCell = cells.find((el) => el.classList.contains('active'));
-  const activeCellIndex = cells.indexOf(activeCell);
-  let index = Math.floor(Math.random() * 16);
-  if(activeCellIndex !== index) {
-    return index;
-  } else {
-    getProperIndex();
-  }
-}
-
-setInterval(() => {
-  const index = getProperIndex();
-  cells.forEach((el) => el.classList.remove('active'));
-  cells[index].classList.add('active');
-  cells[index].appendChild(goblinElement);
-}, 1000);
+const table = new Table(document.querySelector('.table'));
+table.rotate();
